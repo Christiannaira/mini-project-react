f1bebd83566c0fa5910a4a22c4bc50f9c27796e2
jekdiaz <jekdiaz17@gmail.com>
2023-06-07 22:31:01 +0800

    added-bookmarkService

--- src/pages/Childrensbook.jsx
+++ src/pages/Childrensbook.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
-
+import { addToBookmark } from "./bookmarkService";
 
 
 function Childrensbook() {
   const [books, setBooks] = useState([]);
-
   const navigate = useNavigate();
+  const handleBookmark = (book) => {
+    addToBookmark(book);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,32 +29,35 @@ function Childrensbook() {
   return (
     <>
       <section className="featured-books" id="featured-books">
-        <div className="container featured-books-content">
-          <h2>Children's Book</h2>
+      <div className="container featured-books-content">
+        <h2>Children's Book</h2>
 
-          <div className="row mt-3">
-            {books.map((book) => (
-              <div className="book col-lg-2 text-center">
-                <div className="mb-3">
-                  <div className="featured-book-img p-1">
-                    <div className="inner-block p-1">
-                      <img
-                        src={book.volumeInfo.imageLinks.smallThumbnail}
-                        alt={`${book.volumeInfo.title} Book Cover`}
-                       
-                        title={book.volumeInfo.title}
-                        height={"310px"}
-                        width={"100%"}
-                        onClick={()=>navigate(`/bookdetails/${book.id}`)}
-                      />
-                    </div>
+        <div className="row mt-3">
+          {books.map((book) => (
+            <div className="book col-lg-2 text-center" key={book.id}>
+              <div className="mb-3">
+                <div className="featured-book-img p-1">
+                  <div className="inner-block p-1">
+                    <img
+                      src={book.volumeInfo.imageLinks.smallThumbnail}
+                      alt={`${book.volumeInfo.title} Book Cover`}
+                      title={book.volumeInfo.title}
+                      height={"310px"}
+                      width={"100%"}
+                      onClick={() => navigate(`/bookdetails/${book.id}`)}
+                    />
+                    <button
+                      className="bookmark-icon"
+                      onClick={() => handleBookmark(book)}
+                    >Add to Bookmark</button>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      </section>
+      </div>
+    </section>
     </>
   );
 }
---
+++ src/pages/bookmarkService.jsx
@@ -0,0 +1,23 @@
+export const addToBookmark = (book) => {
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+    const newBookmark = {
+      id: book.id,
+      title: book.volumeInfo.title,
+      url: book.volumeInfo.previewLink,
+      image: book.volumeInfo.imageLinks?.thumbnail,
+      author: book.volumeInfo.authors?.join(", "),
+    };
+  
+    const isBookmarked = bookmarks.some(
+      (bookmark) => bookmark.id === newBookmark.id
+    );
+  
+    if (isBookmarked) {
+      alert("This book is already bookmarked.");
+    } else {
+      bookmarks.push(newBookmark);
+      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+      alert("Book has been bookmarked!");
+    }
+  };
+  
